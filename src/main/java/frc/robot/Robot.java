cb49a0259b19e9b7466dbe8472757eb05451f8a3
timesparrow <forrest.tittle@gmail.com>
2020-02-24 15:13:21 -0800

    Added Load Ball Command

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -97,6 +98,7 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
+    m_robotContainer.m_drivetrain.resetDistance();
   }
 
   /**
@@ -105,6 +107,7 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
     new GetColorObjective().schedule();
+    SmartDashboard.putNumber("Distance", m_robotContainer.m_drivetrain.getDistance());
   }
 
   @Override
