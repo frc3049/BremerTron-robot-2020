028fbf368d82671a38fa303a2b83c4f025d323c4
Namo <bentlypamela@gmail.com>
2020-02-24 14:43:01 -0800

    control arm movement and merging with correct ports

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,6 +12,8 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 import frc.robot.commands.GetColorObjective;
+import frc.robot.subsystems.Climber;
+import frc.robot.subsystems.ControlArmMovement;
 import frc.robot.subsystems.Intake;
 
 /**
@@ -22,6 +24,7 @@ import frc.robot.subsystems.Intake;
  */
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
+  public static Climber m_climber = null;
   public static Intake m_intake = null;
   public static RobotContainer m_robotContainer;
   
